d39cf93d75bf28129eee1f2aaae0b93cf07339ad
Thom <Thombog@gmail.com>
2026-03-13 14:08:56 +0100

    Max time hero up by a lot

--- src/Model/AdventureModel.php
+++ src/Model/AdventureModel.php
@@ -170,7 +170,17 @@ class AdventureModel
 
     public function getAdventureExpireTime()
     {
-        return max(7 * 86400 / getGameSpeed(), 18000);
+        // return max(7 * 86400 / getGameSpeed(), 18000);
+        // Always at least 8 hours, then add some time depending on the speed of the server
+        // 1x =  8 + 168 hours  = 7.30 days
+        // 2x =  8 + 84 hours   = 3.84 days
+        // 3x =  8 + 56 hours   = 2.67 days
+        // 5x =  8 + 56 hours   = 1.73 days
+        // 10x = 8 + 16.8 hours = 1.03 days
+        // 20x = 8 + 8.4 hours  = 16.4 hours
+        // 50x = 8 + 11.36 hours= 11.4 hours
+        // 100x= 8 + 1.68 hours = 9.68 hours
+        return (60 * 60 * 8)  + (7 * 86400 / getGameSpeed());
     }
 
     public function addNewUserAdventures($uid)
